c469789f53cf77e0a127bec7e5417f21484ab123
ADoyle <adoyle.h@gmail.com>
2016-02-29 22:20:48 +0800

    update variables

--- rules/variables.js
+++ rules/variables.js
@@ -10,18 +10,18 @@ module.exports = {
         'no-delete-var': 2,
         // disallow labels that share a name with a variable
         'no-label-var': 2,
-        // disallow shadowing of names such as `arguments`
-        'no-shadow-restricted-names': 2,
         // disallow declaration of variables already declared in the outer scope
         'no-shadow': [2, {
             'builtinGlobals': true,
             'hoist': 'all',
             'allow': ['callback'],
         }],
-        // disallow use of undefined when initializing variables
-        'no-undef-init': 2,
+        // disallow shadowing of names such as `arguments`
+        'no-shadow-restricted-names': 2,
         // disallow use of undeclared variables unless mentioned in a `/*global */` block (recommended)
         'no-undef': 2,
+        // disallow use of undefined when initializing variables
+        'no-undef-init': 2,
         // disallow use of `undefined` variable
         'no-undefined': 0,
         // disallow declaration of variables that are not used in the code (recommended)
